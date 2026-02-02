6054606e754e319c3417196cd369ead90c490bab
Maciej Bozetka <mbozetka@gmail.com>
2026-02-02 14:23:49 +0100

    sdk skeleton setup

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Pikaia Android"
+include(":pikaia-app")
+include(":sdk-core")
+include(":sdk-auth")
+include(":sdk-sync")
+include(":sdk-bom")
